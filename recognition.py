afa8eb1dccc7f9269ad6899adc73bf8891c8d16c
Michael M <mike7xyz@gmail.com>
2021-06-12 16:34:45 +0200

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -28,8 +28,7 @@ while True:
     upper_right = upper_left + square_side_length
     lower_right = lower_left + square_side_length
     roi = frame[lower_left:lower_right, upper_left:upper_right]
-    cv.rectangle(frame, (upper_left, lower_left),
-                 (upper_right, lower_right), (252, 0, 0), 0)
+    cv.rectangle(frame, (upper_left, lower_left), (upper_right, lower_right), (252, 0, 0), 0)
 
     # convert roi to HSV and filter hand color
     roi_hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
@@ -50,13 +49,89 @@ while True:
     mask_blurred = cv.medianBlur(mask_closed, 5)
 
     # Compute the contours
-    contours, _ = cv.findContours(
-        mask_blurred, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv.findContours(mask_blurred, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # Find contour of max area (hand)
+    try:
+        hand_contour = max(contours, key=lambda x: cv.contourArea(x))
+
+        # approx the contour a little
+        epsilon = 0.0005 * cv.arcLength(hand_contour, True)
+        approx = cv.approxPolyDP(hand_contour, epsilon, True)
+
+        # make convex hull around hand
+        hull = cv.convexHull(hand_contour)
+
+        # define area of hull and area of hand
+        area_hull = cv.contourArea(hull)
+        area_hand_contour = cv.contourArea(hand_contour)
+
+        # find the percentage of area not covered by hand in convex hull
+        area_ratio = ((area_hull - area_hand_contour) / area_hand_contour) * 100
+
+        # find the defects in convex hull with respect to hand
+        hull = cv.convexHull(approx, returnPoints=False)
+        defects = cv.convexityDefects(approx, hull)
+
+        # finding number of defects due to fingers (= l)
+        l = 0
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(approx[s][0])
+            end = tuple(approx[e][0])
+            far = tuple(approx[f][0])
+            pt = (100, 180)
+
+            # find length of all sides of triangle
+            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            s = (a + b + c) / 2
+            ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+            # distance between point and convex hull
+            d = (2 * ar) / a
+
+            # apply cosine rule here
+            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
+
+            # ignore angles > 90 and ignore points very close to convex hull (generally noise induced points)
+            if angle <= 90 and d > 30:
+                l += 1
+                cv.circle(roi, far, 3, [0, 0, 255], -1)
+
+            # draw lines around hand
+            cv.line(roi, start, end, [255, 0, 0], 2)
+
+        # minimum one defect for hand
+        l += 1
+        (x, y), (MA, ma), angle = cv.fitEllipse(approx)
+        print(f"Defects: {l}\tAngle: {angle}")
+               
+        """ Car Racing Game """
+        if l == 1:
+            # For Linux
+            if (angle >= 150 and angle <= 180) or (angle >= 0 and angle <= 20):
+                print("pressing up")
+                controlling(Directions.UP)
+            elif angle >= 110 and angle < =150:
+                print("pressing left")
+                controlling(Directions.LEFT)
+            elif angle >= 20 and angle< =90:
+                print("pressing right")
+                controlling(Directions.RIGHT)
+            else:
+                print("stopping")
+                controlling(Directions.STOP)
+        else:
+            print("stopping")
+            controlling(Directions.STOP)
+
+    except:
+        pass
 
 
-    # Slack and show
+    # Stack and Show
     color_extraction_stack = stack_images(0.8, [[roi, mask], [roi_hsv, mask_blurred]])
     cv.imshow("Color Extraction Stack", color_extraction_stack)
 
